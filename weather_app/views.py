f313a8ae9512dcb75993ce9b49738a3682a5c628
Joyining <yining1204@gmail.com>
2023-04-11 15:41:03 +0800

    Store api key in .env and handle error

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,31 +1,37 @@
 from django.shortcuts import render
 import json
 import urllib.request
+import urllib.error
+from decouple import config
 
 # Create your views here.
 
 
 def index(request):
+    city = ''
+    data = {}
+    error_message = ''
     if request.method == 'POST':
         city = request.POST['city']
-        response = urllib.request.urlopen(
-            'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=30d37d80cfdaf0940fe4bc711dfa8bbd')
-        print(response)
-        # use json.load to convert json string to python dictionary.
-        original_data = json.load(response)
-        print(type(original_data))  # <class 'dict'>
-        data = {
-            'country_code': str(original_data['sys']['country']),
-            'coordinate': str(original_data['coord']['lon']) + ' ' + str(original_data['coord']['lat']),
-            'temp': str(original_data['main']['temp']) + 'k',
-            'pressure': str(original_data['main']['pressure']),
-            'humidity': str(original_data['main']['humidity']),
-        }
-    else:
-        city = ''
-        data = {}
+        try:
+            response = urllib.request.urlopen(
+                'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+config('WEATHER_API_KEY'))
+            print(response)
+            # use json.load to convert json string to python dictionary.
+            original_data = json.load(response)
+            print(type(original_data))  # <class 'dict'>
+            data = {
+                'country_code': str(original_data['sys']['country']),
+                'coordinate': str(original_data['coord']['lon']) + ' ' + str(original_data['coord']['lat']),
+                'temp': str(original_data['main']['temp']) + 'k',
+                'pressure': str(original_data['main']['pressure']),
+                'humidity': str(original_data['main']['humidity']),
+            }
+        except urllib.error.HTTPError as error:
+            error_message = error
     context = {
         'city': city,
-        'data': data
+        'data': data,
+        'error_message': error_message,
     }
     return render(request, 'index.html', context)
